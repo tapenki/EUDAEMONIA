4cbd8338da384a1139da74ad3d705b970e15ef5d
Tapenki <113533673+Tapenki@users.noreply.github.com>
2025-10-05 22:19:23 +0300

    finishing touches

--- regions/olethros/bomb_debree/bomb_debree.gd
+++ regions/olethros/bomb_debree/bomb_debree.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	ability_handler.before_self_death.connect(before_self_death)
 
 func damage_taken(_source, _damage) -> void:
-	if not get_node("Lifetime").running and ability_handler.owner.alive:
+	if not get_node("Lifetime").running and alive:
 		get_node("Lifetime").start()
 		ability_handler.owner.get_node("AnimationPlayer").play("PRIMED")
 
@@ -16,15 +16,17 @@ func before_self_death(modifiers) -> void:
 	modifiers["prevented"] = true
 
 func loosen():
-	ability_handler.owner.get_node("AnimationPlayer").play("PRIMED")
+	if not get_node("Lifetime").running and alive:
+		ability_handler.owner.get_node("AnimationPlayer").play("PRIMED")
 	super()
 
 func kill():
-	var explosion_instance = ability_handler.make_projectile(explosion_scene, 
-	global_position, ## position
-	3, ## inheritance
-	Vector2()) ## velocity
-	explosion_instance.scale_multiplier = 2
-	get_node("/root/Main/Projectiles").add_child(explosion_instance)
-	get_node("/root/Main").play_sound("Explosion")
+	if alive:
+		var explosion_instance = ability_handler.make_projectile(explosion_scene, 
+		global_position, ## position
+		3, ## inheritance
+		Vector2()) ## velocity
+		explosion_instance.scale_multiplier = 2
+		get_node("/root/Main/Projectiles").add_child(explosion_instance)
+		get_node("/root/Main").play_sound("Explosion")
 	super()
