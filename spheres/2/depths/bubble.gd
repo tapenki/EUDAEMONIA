5761eb22d09554d7efe49fffba853b4a13e673dd
Tapenki <113533673+Tapenki@users.noreply.github.com>
2026-03-23 22:58:21 +0300

    diver 2

--- spheres/2/depths/bubble.gd
+++ spheres/2/depths/bubble.gd
@@ -1,4 +1,24 @@
 extends Sprite2D
 
+@export var ability_handler: Node
+@export var bullet: PackedScene
+
 func _physics_process(_delta: float) -> void:
 	scale = Vector2(0.8, 0.8) * (1 - sin(Time.get_ticks_msec()*0.005) * 0.1)
+
+func _ready() -> void:
+	ability_handler.damage_taken.connect(damage_taken)
+
+func damage_taken(_damage) -> void:
+	if visible:
+		visible = false
+		var angle = randf_range(0, TAU)
+		const count = 3
+		for repeat in count:
+			var bullet_instance = ability_handler.make_projectile(bullet, 
+			global_position, 
+			2,
+			Vector2.from_angle(angle + (TAU / count * repeat)) * 300)
+			bullet_instance.get_node("Lifetime").start(1)
+			get_node("/root/Main/Projectiles").add_child(bullet_instance)
+		get_node("/root/Main").spawn_particles(get_node("/root/Main/Particles/Impact"), 5, global_position, 2, Config.get_team_color(ability_handler.owner.group, "secondary"))
